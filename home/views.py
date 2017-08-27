85643b5f9bbc1acfe2c71b92d0fb922510516757
Cessros <cessrosman88top727start@gmail.com>
2017-08-27 11:49:01 -0400

    created and updated timeoption

--- home/views.py
+++ home/views.py
@@ -22,6 +22,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
@@ -37,20 +38,57 @@ class TimeoutOptionView(APIView):
         token = Token.objects.get(key=request.auth)
         curr_user_id = token.user_id
         curr_timeout = request.data['timeout']
-        last_user_id = TimeoutOption.objects.all().last().user_id
-        last_time_out = TimeoutOption.objects.all().last().timeout
-
-        if not curr_timeout:
-            return Response('Please set time option', status=status.HTTP_404_NOT_FOUND)
-
-        if not last_time_out:
+        if TimeoutOption.objects.all():
+            if TimeoutOption.objects.get(user_id=token.user_id):
+                timeout = TimeoutOption.objects.get(user_id=token.user_id)
+                timeout.timeout = curr_timeout
+            else:
+                timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
+        else:
             timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
-            timeout.save()
-            return Response('success', status=status.HTTP_200_OK)
 
-        if curr_user_id == last_user_id and int(curr_timeout) == last_time_out:
-            return Response('This value already exist', status=status.HTTP_200_OK)
-
-        timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
         timeout.save()
         return Response('success', status=status.HTTP_200_OK)
+
+
+
+        # last_user_id = TimeoutOption.objects.all().last().user_id
+        # last_time_out = TimeoutOption.objects.all().last().timeout
+
+        # if not last_time_out:
+        #     timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
+        #     timeout.save()
+        #     return Response('New created. Success', status=status.HTTP_200_OK)
+        # if last_time_out:
+        #     last_time_out = curr_timeout
+        #     return Response('success', status=status.HTTP_200_OK)
+
+
+        # # timeout = TimeoutOption.objects.get(user_id=curr_user_id)
+        # # if timeout
+        # #     timeout.timeout =
+        # # else
+        #
+        # if not curr_timeout:
+        #     return Response('Please set time option', status=status.HTTP_404_NOT_FOUND)
+        #
+        # if not last_time_out:
+        #     timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
+        #     timeout.save()
+        #     return Response('success', status=status.HTTP_200_OK)
+        #
+        # if curr_user_id == last_user_id and int(curr_timeout) == last_time_out:
+        #     return Response('This value already exist', status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
+
+
