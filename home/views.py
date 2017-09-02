c505e82fa5785e583c9c043b9c5bed34d58dfd27
Cessros <cessrosman88top727start@gmail.com>
2017-09-02 00:24:58 -0400

    updated Result 750

--- home/views.py
+++ home/views.py
@@ -18,16 +18,6 @@ import json
 import pymongo
 from pymongo import MongoClient
 
-# mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
-# connection = MongoClient(host=mongoserver_uri)
-# db = connection['cc_accounts']
-# collection = db['LANDON_coinigy_account']
-# cursor = collection.find({})
-# for document in cursor:
-#     context = {'context': document}
-
-
-
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -65,15 +55,50 @@ class TimeoutOptionView(APIView):
         return Response('success', status=status.HTTP_200_OK)
 
 
+# class RetrieveDataTimeFrameView(APIView):
+#     parser_classes = (FormParser,)
+#
+#     def post(self, request, format=None):
+#         token = Token.objects.get(key=request.auth)
+#         curr_user_id = token.user_id
+#         curr_timeout = request.data['timeout']
+#         json_data = []
+#         mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
+#         connection = MongoClient(host=mongoserver_uri)
+#         db = connection['cc_accounts']
+#         collection = db['LANDON_coinigy_account']
+#
+#         if TimeoutOption.objects.all():
+#             if TimeoutOption.objects.get(user_id=token.user_id):
+#                 timeout = TimeoutOption.objects.get(user_id=token.user_id)
+#                 timeout.timeout = curr_timeout
+#             else:
+#                 timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
+#         else:
+#             timeout = TimeoutOption(user_id=curr_user_id, timeout=curr_timeout)
+#         timeout.save()
+#
+#         data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(540))
+#         for datum in data:
+#             json_data.append({'id': str(datum['_id']),
+#                               'balance_curr_code': datum['balance_curr_code'],
+#                               'balance_amount_avail': datum['balance_amount_avail'],
+#                               'balance_amount_held': datum['balance_amount_held'],
+#                               'balance_amount_total': datum['balance_amount_total'],
+#                               'btc_balance': datum['btc_balance'],
+#                               'last_price': datum['last_price'],
+#                               'time': datum['time']})
+#         return Response(json_data, status=status.HTTP_200_OK)
+
+
 class RetrieveDataView(APIView):
     def get(self, request, format=None):
         json_data = []
-        newdata = {}
         mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
         connection = MongoClient(host=mongoserver_uri)
         db = connection['cc_accounts']
         collection = db['LANDON_coinigy_account']
-        data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(540))
+        data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(750))
         for datum in data:
             json_data.append({'id': str(datum['_id']),
                               'balance_curr_code': datum['balance_curr_code'],
@@ -85,26 +110,65 @@ class RetrieveDataView(APIView):
                               'time': datum['time']})
         return Response(json_data, status=status.HTTP_200_OK)
 
-
-class RetrieveLatestDataView(APIView):
-    def get(self, request, format=None):
-        json_data = []
-        newdata = {}
-        mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
-        connection = MongoClient(host=mongoserver_uri)
-        db = connection['cc_accounts']
-        collection = db['LANDON_coinigy_account']
-        data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(18))
-        for datum in data:
-            json_data.append({'id': str(datum['_id']),
-                              'balance_curr_code': datum['balance_curr_code'],
-                              'balance_amount_avail': datum['balance_amount_avail'],
-                              'balance_amount_held': datum['balance_amount_held'],
-                              'balance_amount_total': datum['balance_amount_total'],
-                              'btc_balance': datum['btc_balance'],
-                              'last_price': datum['last_price'],
-                              'time': datum['time']})
-        return Response(json_data, status=status.HTTP_200_OK)
+#
+# class RetrieveLatestDataView(APIView):
+#     def get(self, request, format=None):
+#         json_data = []
+#         mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
+#         connection = MongoClient(host=mongoserver_uri)
+#         db = connection['cc_accounts']
+#         collection = db['LANDON_coinigy_account']
+#         data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(25))
+#         for datum in data:
+#             json_data.append({'id': str(datum['_id']),
+#                               'balance_curr_code': datum['balance_curr_code'],
+#                               'balance_amount_avail': datum['balance_amount_avail'],
+#                               'balance_amount_held': datum['balance_amount_held'],
+#                               'balance_amount_total': datum['balance_amount_total'],
+#                               'btc_balance': datum['btc_balance'],
+#                               'last_price': datum['last_price'],
+#                               'time': datum['time']})
+#         return Response(json_data, status=status.HTTP_200_OK)
+#
+#
+# class RetrieveDataViewFiveMin(APIView):
+#     def get(self, request, format=None):
+#         json_data = []
+#         mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
+#         connection = MongoClient(host=mongoserver_uri)
+#         db = connection['cc_accounts']
+#         collection = db['LANDON_coinigy_account']
+#         data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(750))
+#         for datum in data:
+#             json_data.append({'id': str(datum['_id']),
+#                               'balance_curr_code': datum['balance_curr_code'],
+#                               'balance_amount_avail': datum['balance_amount_avail'],
+#                               'balance_amount_held': datum['balance_amount_held'],
+#                               'balance_amount_total': datum['balance_amount_total'],
+#                               'btc_balance': datum['btc_balance'],
+#                               'last_price': datum['last_price'],
+#                               'time': datum['time']})
+#         return Response(json_data, status=status.HTTP_200_OK)
+#
+#
+# class RetrieveDataViewFifteenMin(APIView):
+#     def get(self, request, format=None):
+#         json_data = []
+#         mongoserver_uri = "mongodb://Readuser:jbh4S3pCpTGCdIGGVOU6@10.8.0.2:27017/admin"
+#         connection = MongoClient(host=mongoserver_uri)
+#         db = connection['cc_accounts']
+#         collection = db['LANDON_coinigy_account']
+#         data = list(collection.find({}).sort('_id', pymongo.DESCENDING).limit(540))
+#         for datum in data:
+#             json_data.append({'id': str(datum['_id']),
+#                               'balance_curr_code': datum['balance_curr_code'],
+#                               'balance_amount_avail': datum['balance_amount_avail'],
+#                               'balance_amount_held': datum['balance_amount_held'],
+#                               'balance_amount_total': datum['balance_amount_total'],
+#                               'btc_balance': datum['btc_balance'],
+#                               'last_price': datum['last_price'],
+#                               'time': datum['time']})
+#         return Response(json_data, status=status.HTTP_200_OK)
 
 
 
